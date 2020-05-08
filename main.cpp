36ca9274a99c2ff55af413026bb5f09caae57bdc
Bers <berserkingyadis@gmail.com>
2020-05-08 09:56:47 +0200

    SDL2 included

--- main.cpp
+++ main.cpp
@@ -1,11 +1,55 @@
 #define VERSION "0.2.1"
 
-#include <iostream>
+#include <stdio.h>
+
+#include "SDL.h"
+
+
+//initializes SDL
+bool init();
+
+//loads media
+bool loadMedia();
+
+//frees media and shuts down sdl
+void close();
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
 
 int main(int argc, char *argv[]) {
 
-	std::cout << "Hello World :)" << std::endl;
-
 	return 0;
+}
 
+bool init() {
+	bool success = true;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
+	}
+
+	return success;
 }
